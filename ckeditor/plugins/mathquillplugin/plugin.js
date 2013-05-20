4c0613050ad94a67805fc3ea0e371342e4796536
Dan Murphy <daniel.murphy.jr@gmail.com>
2013-05-20 13:39:28 -0400

    Mathquill CKEditor Plugin inserts selected text
    The Mathquill CKEditor plugin button now uses the selected text as the
    starting point for the mathquill editible box (and will mathquill-ify it
    too). It also handles when there is no selection, or all white space is
    selected. RE: Issue #3

--- ckeditor/plugins/mathquillplugin/plugin.js
+++ ckeditor/plugins/mathquillplugin/plugin.js
@@ -4,8 +4,18 @@ CKEDITOR.plugins.add( 'mathquillplugin', {
         //Plugin logic goes here.
         editor.addCommand( 'insertMathquill', {
             exec: function( editor ) {
+
+                // get the selected text - it will always return type string, even if nothing is selected
+                var selectedText = editor.getSelection().getSelectedText();
+
+                // Check for an empty string or all whitespace using jQuery trim method
+                if (!selectedText || $.trim(selectedText) == '') {
+                    // If empty or all whitespace set initial math value
+                    selectedText = 'Insert-Math-Here';
+                }
+
                 // store the created element in a variable so I can then use jQuery and the Mathquill plugin on it.
-                editor.insertHtml( '<span id="tempID">x^2</span>' );
+                editor.insertHtml( '<span id="tempID">' + selectedText + '</span>' );
                 $('#tempID').mathquill('editable');
             }
         });
