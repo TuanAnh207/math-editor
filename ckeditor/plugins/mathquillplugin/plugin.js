b4c8c8c37b53aa6754675626a91736a954355a5d
Dan Murphy <daniel.murphy.jr@gmail.com>
2013-05-22 10:40:00 -0400

    Multiple Mathquill instances work, but unfinished
    This is a temporary comitt, I have multiple instances of Mathquill
    editable boxes working, but I want to try another approach to inserting
    the span into the CKEditor so that it works with no selections without all
    the if/else logic. RE: Issue #3

--- ckeditor/plugins/mathquillplugin/plugin.js
+++ ckeditor/plugins/mathquillplugin/plugin.js
@@ -12,11 +12,22 @@ CKEDITOR.plugins.add( 'mathquillplugin', {
                 if (!selectedText || $.trim(selectedText) == '') {
                     // If empty or all whitespace set initial math value
                     selectedText = 'Insert-Math-Here';
-                }
 
-                // store the created element in a variable so I can then use jQuery and the Mathquill plugin on it.
-                editor.insertHtml( '<span id="tempID">' + selectedText + '</span>' );
-                $('#tempID').mathquill('editable');
+                    // add a span with a temporary ID used for the jQuery selector
+                    editor.insertHtml( '<span id="tempID">' + selectedText + '</span>' );
+
+                    // convert to a Mathquill editable box and then remove the temporary ID
+                    $('#tempID').mathquill('editable').removeAttr('id');
+                }
+                else {
+                    // If text was selected
+
+                    // add a span with a temporary ID just for the jQuery selector
+                    editor.insertHtml( '<span id="tempID">' + selectedText + '</span>' );
+
+                    // convert to a Mathquill editable box and then remove the temporary ID
+                    $('#tempID').mathquill('editable').removeAttr('id');
+                }
             }
         });
         editor.ui.addButton( 'Mathquill_Plugin', {
